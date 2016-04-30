36521006edca0127c6a191db50b00fef928ee5fb
lorenzo-carbonell <lorenzo.carbonell.cerezo@gmail.com>
2016-04-30 12:27:18 +0200

    Updated translations and add font option

--- src/comun.py
+++ src/comun.py
@@ -3,7 +3,7 @@
 #
 # comun.py
 #
-# Copyright (C) 2012 Lorenzo Carbonell
+# Copyright (C) 2012 - 2016 Lorenzo Carbonell
 # lorenzo.carbonell.cerezo@gmail.com
 #
 # This program is free software: you can redistribute it and/or modify
@@ -22,58 +22,50 @@
 #
 #
 
-__author__ = 'Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>'
-__date__ ='$12/08/2011'
-__copyright__ = 'Copyright (c) 2011 Lorenzo Carbonell'
-__license__ = 'GPLV3'
-__url__ = 'http://www.atareao.es'
-__version__ = '0.0.1.0'
-
 import os
 import locale
 import gettext
-######################################
+
 
 def is_package():
     return __file__.find('src') < 0
 
-######################################
+PARAMS = {'version': '',
+          'last_dir': '',
+          'last_filename': '',
+          'filename1': '',
+          'filename2': '',
+          'filename3': '',
+          'filename4': '',
+          'width': 900,
+          'height': 600,
+          'toolbar': True,
+          'statusbar': True,
+          'markdown_editor.show_line_numbers': True,
+          'markdown_editor.show_line_marks': True,
+          'markdown_editor.spaces': True,
+          'markdown_editor.tab_width': 4,
+          'markdown_editor.auto_indent': True,
+          'markdown_editor.highlight_current_line': True,
+          'markdown_editor.font': 'ubuntu 14',
+          'html_viewer.show_line_numbers': True,
+          'html_viewer.show_line_marks': True,
+          'html_viewer.spaces': True,
+          'html_viewer.tab_width': 4,
+          'html_viewer.auto_indent': True,
+          'html_viewer.highlight_current_line': True,
+          'html_viewer.preview_theme':
+          '/opt/extras.ubuntu.com/utext/share/utext/themes/default',
+          'autosave': False,
+          'spellcheck': False,
+          'mathjax': False,
+          }
 
-PARAMS = {	'version':'',			
-			'last_dir':'',
-			'last_filename':'',
-			'filename1':'',
-			'filename2':'',
-			'filename3':'',
-			'filename4':'',
-			'width':900,
-			'height':600,
-			'toolbar':True,
-			'statusbar':True,
-			'markdown_editor.show_line_numbers':True,
-			'markdown_editor.show_line_marks':True,
-			'markdown_editor.spaces':True,
-			'markdown_editor.tab_width':4,
-			'markdown_editor.auto_indent':True,
-			'markdown_editor.highlight_current_line':True,
-			'html_viewer.show_line_numbers':True,
-			'html_viewer.show_line_marks':True,
-			'html_viewer.spaces':True,
-			'html_viewer.tab_width':4,
-			'html_viewer.auto_indent':True,
-			'html_viewer.highlight_current_line':True,
-			'html_viewer.preview_theme':'/opt/extras.ubuntu.com/utext/share/utext/themes/default',
-			'autosave':False,
-			'spellcheck':False,
-			'mathjax':False,
-}
 
-
-VERSION = __version__
 APP = 'utext'
 APP_CONF = APP + '.conf'
 APPNAME = 'uText'
-CONFIG_DIR = os.path.join(os.path.expanduser('~'),'.config')
+CONFIG_DIR = os.path.join(os.path.expanduser('~'), '.config')
 CONFIG_APP_DIR = os.path.join(CONFIG_DIR, APP)
 CONFIG_FILE = os.path.join(CONFIG_APP_DIR, APP_CONF)
 TOKEN_FILE = os.path.join(CONFIG_APP_DIR, 'token')
@@ -86,7 +78,7 @@ if is_package():
     THEMESDIR = os.path.join(APPDIR, 'themes')
     ICONDIR = os.path.join(APPDIR, 'icons')
     SOCIALDIR = os.path.join(APPDIR, 'social')
-    CHANGELOG = os.path.join(APPDIR,'changelog')
+    CHANGELOG = os.path.join(APPDIR, 'changelog')
 else:
     ROOTDIR = os.path.dirname(__file__)
     LANGDIR = os.path.normpath(os.path.join(ROOTDIR, '../po'))
@@ -94,23 +86,23 @@ else:
     ICONDIR = os.path.normpath(os.path.join(ROOTDIR, '../data/icons'))
     SOCIALDIR = os.path.normpath(os.path.join(ROOTDIR, '../data/social'))
     DEBIANDIR = os.path.normpath(os.path.join(ROOTDIR, '../debian'))
-    CHANGELOG = os.path.join(DEBIANDIR,'changelog')
+    CHANGELOG = os.path.join(DEBIANDIR, 'changelog')
     APPDIR = ROOTDIR
 ####
-f = open(CHANGELOG,'r')
+f = open(CHANGELOG, 'r')
 line = f.readline()
 f.close()
-pos=line.find('(')
-posf=line.find('-',pos)
+pos = line.find('(')
+posf = line.find('-', pos)
 VERSION = line[pos+1:posf].strip()
 if not is_package():
-	VERSION = VERSION + '-src'
+    VERSION = VERSION + '-src'
 ####
-ICON = os.path.join(ICONDIR,'utext.svg')
+ICON = os.path.join(ICONDIR, 'utext.svg')
 current_locale, encoding = locale.getdefaultlocale()
 try:
-	language = gettext.translation(APP, LANGDIR, [current_locale])
-	language.install()
-	_ = language.gettext
+    language = gettext.translation(APP, LANGDIR, [current_locale])
+    language.install()
+    _ = language.gettext
 except:
-	_ = str
+    _ = str
